1599bc6bc0d41d9d90a7ccda8c7e3c7ea032eee7
Orlando Gomes <orlandosjm@yahoo.com.br>
2023-03-07 15:05:54 -0300

    add router

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
+import { BrowserRouter } from 'react-router-dom'
 import { ThemeProvider } from 'styled-components'
-import { Home } from './pages/Home'
+import { Router } from './Router'
 
 import { GlobalStyle } from './styles/global'
 import { defaultTheme } from './styles/themes/default'
@@ -8,7 +9,10 @@ import { defaultTheme } from './styles/themes/default'
 function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
-      <Home />
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
+
       <GlobalStyle />
     </ThemeProvider>
   )
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,22 +1,10 @@
-import { MapPin, ShoppingCart } from '@phosphor-icons/react'
-import logo from '../../assets/logo.svg'
-import { HeaderContainer, LocationAndCartContainer } from './styles'
+import { ShoppingCart } from '@phosphor-icons/react'
+import { Header } from '../../components/Header'
 
 export function Home() {
   return (
     <>
-      <HeaderContainer>
-        <img src={logo} alt="logo" />
-        <LocationAndCartContainer>
-          <div>
-            <MapPin size={22} weight="fill" />
-            <span>Porto Alegre, RS</span>
-          </div>
-          <div>
-            <ShoppingCart size={22} weight="fill" />
-          </div>
-        </LocationAndCartContainer>
-      </HeaderContainer>
+      <Header />
       <main>
         <section>
           <article>
