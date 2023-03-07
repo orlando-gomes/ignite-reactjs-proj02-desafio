75b59cf1a5682e4e4c5f58878e6a111db529310b
Orlando Gomes <orlandosjm@yahoo.com.br>
2023-03-07 09:40:40 -0300

    first commit - add global style

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+// import { useState } from 'react'
+
+import { GlobalStyle } from './styles/global'
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1>Coffee Delivery</h1>
+      <GlobalStyle />
+    </>
+  )
+}
+
+export default App
